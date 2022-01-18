4b36e6d23c3e0cdec31fdca1f6c4cbd04e171abe
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-01-17 21:03:54 -0800

    UseDarkMode Hook
    - Added Dark Styles
    - Added Dark Mode Hook

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import { BsSunFill, BsMoonStars, BsInstagram, BsTwitter, BsGithub } from 'react-icons/bs';
 
 import Image from 'next/image'
 import Link from 'next/link'
 
-import OutlineLogo from '../public/images/white_logo_outline.png';
+import useDarkMode from '../hooks/useDarkMode';
+import OutlineLogo from '../public/images/white_logo.png';
 import Logo from '../public/images/logo.png';
 import Banner from '../public/images/banner.png';
+import WhiteBanner from '../public/images/white_banner.png';
 
 const NavLink = React.forwardRef(({ onClick, href, children, className}, ref) => {
     return (
@@ -17,18 +20,26 @@ const NavLink = React.forwardRef(({ onClick, href, children, className}, ref) =>
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const [colorTheme, setTheme] = useDarkMode();
 
     const handleMenu = () => {
         setShowMenu(!showMenu);
     }
 
+    const handleTheme = () => {
+        setTheme(colorTheme);
+        console.log(colorTheme)
+    }
+
     return (
         // Header starts here
-        <div className='bg-white p-4 md:py-10 md:px-20'>
+        <div className='bg-white dark:bg-slate-800 p-4 md:py-10 md:px-20'>
             <header className="mx-auto container px-4 xl:px-0">
                 <div className="flex items-center justify-between">
                     <Link href="/" passHref>
-                        <NavLink className="w-36"><Image className="object-cover" src={Logo} alt="Logo" /></NavLink>
+                        <NavLink className="w-36">
+                            <Image className="object-cover" src={colorTheme === 'light' ? OutlineLogo : Logo} alt="Logo" />
+                        </NavLink>
                     </Link>
                     <div className="">
                         <div onClick={handleMenu} className="sm:block md:hidden lg:hidden text-gray-500 hover:text-gray-700 focus:text-gray-700 focus:outline-none">
@@ -47,7 +58,7 @@ const Header = () => {
                                     <line x1="6" y1="6" x2="18" y2="18" />
                                 </svg>
                             </div>
-                            <ul className="flex md:flex text-3xl md:text-base lg:text-base lg:flex flex-col md:flex-row lg:flex-row justify-center items-center fixed md:relative lg:relative top-0 bottom-0 left-0 right-0 bg-white z-20">
+                            <ul className="flex md:flex text-3xl md:text-base lg:text-base lg:flex flex-col md:flex-row lg:flex-row justify-center items-center fixed md:relative lg:relative top-0 bottom-0 left-0 right-0 z-20">
                                 <li className="md:mr-8 lg:mr-10 flex items-center">
                                     <span className="text-gray-800 hover:text-gray-600 pt-10 md:pt-0 cursor-pointer" >About</span>
                                 </li>
@@ -56,42 +67,39 @@ const Header = () => {
                     </div>
                     <div className='hidden md:flex justify-start sm:justify-start xl:justify-end'>
                         <Link href="/" passHref>
-                            <NavLink className="p-4 stroke-indigo-500 hover:stroke-indigo-300">
-                                <svg aria-label="Instagram" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-instagram">
-                                    <rect x={2} y={2} width={20} height={20} rx={5} ry={5} />
-                                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-                                </svg>
+                            <NavLink className="p-4 text-gray-700 dark:text-violet-500">
+                                <BsInstagram className='h-6 w-6' />
                             </NavLink>
                         </Link>
                         <Link href="/" passHref>
-                            <NavLink className="p-4 stroke-indigo-500 hover:stroke-indigo-300">
-                                <svg aria-label="Twitter" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter">
-                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-                                </svg>
+                            <NavLink className="p-4 text-gray-700 dark:text-violet-500">
+                                <BsTwitter className='h-6 w-6' />
                             </NavLink>
                         </Link>
                         <Link href="/" passHref>
-                            <NavLink className="p-4 stroke-indigo-500 hover:stroke-indigo-300">
-                                <svg aria-label="Dribble" xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-dribbble" width={24} height={24} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                    <circle cx={12} cy={12} r={9} />
-                                    <path d="M9 3.6c5 6 7 10.5 7.5 16.2" />
-                                    <path d="M6.4 19c3.5-3.5 6-6.5 14.5-6.4" />
-                                    <path d="M3.1 10.75c5 0 9.814-.38 15.314-5" />
-                                </svg>
+                            <NavLink className="p-4 text-gray-700 dark:text-violet-500">
+                                <BsGithub className='h-6 w-6' />
                             </NavLink>
                         </Link>
+                        <button 
+                            onClick={handleTheme}
+                            className='transition duration-150 ease-in-out bg-gray-600 hover:bg-gray-700 rounded-full text-yellow-300 px-5 py-2 text-sm'
+                        >
+                            {colorTheme === 'dark' ? <BsMoonStars /> : <BsSunFill /> }
+                        </button>
                     </div>
                 </div>
             </header>
             <section className="mt-8 sm:mt-24 lg:mt-56 mx-auto container py-12 lg:px-4 xl:px-0 mx-auto container ">
                 <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row">
                     <div className="flex flex-col lg:w-6/12 md:w-6/12 px-4 lg:px-0 justify-center items-start">
-                        <p className="text-2xl text-gray-800 font-light">Hello there! I’am <span className="text-indigo-700">Omar Montoya</span> and</p>
-                        <h1 className="text-5xl sm:text-6xl md:text-4xl lg:text-6xl font-extrabold leading-tight text-gray-800">I’ll Help You Build Your Dream</h1>
-                        <p className="text-lg text-gray-600 font-light leading-relaxed pt-8">In quo quaerimus, non provident, similique sunt in voluptate et inter argumentum conclusionemque. Tum dicere exorsus est et quasi architecto beatae vitae dicta sunt, explicabo nemo. Certe inquam, pertinax non emolumento</p>
-                        <button className="bg-gradient-to-r from-indigo-500 via-indigo-400 to-blue-400 hover:from-indigo-600 hover:to-blue-400 p-5 text-white text-2xl flex items-center text-xl custom-foreground-color font-regular custom-border-bottom focus:outline-none rounded-xl mt-10">
+                        <p className="text-2xl text-gray-800 dark:text-white font-light">Hello there! I’am <span className="text-indigo-700 dark:text-violet-700">Omar Montoya</span> and</p>
+                        <h1 className="text-5xl sm:text-6xl md:text-4xl lg:text-6xl font-extrabold leading-tight text-gray-800 dark:text-white">I’ll Help You Build Your Dream</h1>
+                        <p className="text-lg text-gray-600 font-light leading-relaxed pt-8 dark:text-white">In quo quaerimus, non provident, similique sunt in voluptate et inter argumentum conclusionemque. Tum dicere exorsus est et quasi architecto beatae vitae dicta sunt, explicabo nemo. Certe inquam, pertinax non emolumento</p>
+                        <button 
+                            className="bg-gradient-to-r from-indigo-500 via-indigo-400 to-blue-400 
+                                        hover:from-indigo-600 hover:to-blue-400 p-5 text-white text-2xl flex items-center text-xl custom-foreground-color font-regular custom-border-bottom focus:outline-none rounded-xl mt-10 
+                                        dark:bg-gradient-to-r dark:from-violet-500 dark:to-violet-900 dark:hover:from-violet-600 hover:to-violet-900">
                             Connect with me
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-narrow-right" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" />
