a3e9a08200696c898353773bc181fe21b10106bd
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-01-18 00:19:41 -0800

    Added Project Dark Stlyes

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,9 +4,9 @@ import { projects } from '../constants/constants';
 const Projects = () => {
     console.log(projects);
     return (
-        <section className="p-4 md:py-10 md:px-20">
+        <section className="p-4 md:py-10 md:px-20 dark:bg-slate-800">
             <div className="mx-auto container py-12 px-4 lg:px-0 space-y-4">
-                <h2 className="lg:text-5xl text-2xl text-left f-m-w text-gray-800 font-bold">Projects</h2>
+                <h2 className="lg:text-5xl text-2xl text-left f-m-w text-gray-800 dark:text-white font-bold">Projects</h2>
                 <Divider />
                 <div className="flex flex-wrap items-center lg:justify-between justify-center">
                     {projects.map((project) => (<SingleProject key={project.id} {...project} />))}
--- components/SingleProject.jsx
+++ components/SingleProject.jsx
@@ -2,16 +2,16 @@ import { BsGithub, BsFillCaretRightFill } from 'react-icons/bs';
 
 const SingleProject = ({ id, image, title, description, tags = [], source, visit }) => {
     return (        
-        <div className="mx-2 w-72 lg:mb-0 mb-8" key={id}>
+        <div className="mx-2 w-72 lg:mb-0 mb-8 shadow shadow-lg" key={id}>
             <div>
                 <img src={image} className="w-full object-cover h-44" />
             </div>
-            <div className="bg-gray-100">
+            <div className="bg-gray-100 dark:bg-slate-700">
                 <div className="p-4">
                     <div className="flex items-center">
-                        <h2 className="text-lg font-semibold">{title}</h2>
+                        <h2 className="text-lg font-semibold dark:text-white">{title}</h2>
                     </div>
-                    <p className="text-xs text-gray-600 mt-2">{description}</p>
+                    <p className="text-xs text-gray-600 mt-2 dark:text-gray-300">{description}</p>
                     <div className="flex mt-4">
                         {tags.map((tag, i)=> (
                             <div key={i}>
@@ -20,13 +20,13 @@ const SingleProject = ({ id, image, title, description, tags = [], source, visit
                         ))}
                     </div>
                     <div className="flex items-center justify-between py-4">
-                        <a href={visit} className="mx-2 my-2 flex items-center bg-gradient-to-l from-indigo-600 to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 hover:opacity-75 text-white p-2 text-sm rounded">
+                        <a href={visit} className="flex items-center bg-gradient-to-l from-indigo-600 to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 hover:opacity-75 text-white p-2 text-sm rounded">
                             Demo
                             <span className="h-4 w-4 ml-2">
                                 <BsFillCaretRightFill />
                             </span>
                         </a>
-                        <a href={source} className="mx-2 my-2 flex items-center bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 p-2 text-sm">
+                        <a href={source} className="flex items-center bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 p-2 text-sm">
                             Source Code
                             <span className="h-4 w-4 ml-2">
                                 <BsGithub />
